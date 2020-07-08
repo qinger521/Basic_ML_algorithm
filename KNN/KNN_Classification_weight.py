6e87e5b6abce3ca0fe84bd6e0a066db0598f1476
qinger521 <15610319606@163.com>
2020-07-08 15:38:47 +0800

    KNN——regression

---
+++ KNN/KNN_Classification_weight.py
@@ -0,0 +1,112 @@
+import numpy as np
+import pandas as pd
+import matplotlib as mpl
+import matplotlib.pyplot as plt
+
+# 读取鸢尾花数据集
+data = pd.read_csv(r"iris.arff.csv") # 有一个参数header 表示第几行是标题行，如果数据集没有标题行则为None
+print(data)
+
+# data.head() 表示显示前几行 默认5行 , 也可指定显示几行
+# data.tail() 表示显示末尾几行 默认5行 , 也可指定显示几行
+# data.sample() 表示随机抽取样本 默认1个，也可指定抽取几条
+
+# 对class的文本值进行映射,文本->数字
+data["class"] = data["class"].map({"Iris-virginica":0,"Iris-setosa":1,"Iris-versicolor":2})
+
+# 删除数据集中的一列 data.drop("id",axis=1,inplace=True) 或者 data = data.drop("id",axis=1)
+
+# 去除重复的数据
+data.drop_duplicates(inplace=True)
+
+# 查看各个类别有多少记录
+data["class"].value_counts()
+print(data)
+
+class KNN:
+    '''
+        使用python实现K近邻算法,并考虑距离权重。（实现分类）
+    '''
+
+    def __init__(self,k):
+        '''
+        :param k: int 邻居的个数
+        :return:
+        '''
+        self.k = k
+
+    def fit(self,X,y):
+        '''
+        KNN为惰性学习算法，他将数据集先保存起来，在新的数据到达时在进行判断与分类
+        :param X: 类数组类型 [样本数量，属性数目]
+        :param y: 类数组类型 [样本数量]
+        :return:
+        '''
+
+        # 不能直接保存X，因为X的类型不统一，在此将其直接转换为np.array数组的形式
+        self.X = np.asarray(X)
+        self.y = np.asarray(y)
+
+    def predict(self,X):
+        '''
+        根据参数传递的样本，对样本进行预测
+        :param X: 类数组类型 [样本数量，属性数目]
+        :return: 数组类型 预测结果
+        '''
+        X = np.asarray(X)
+        result = []
+        # 对ndarray数组进行遍历，每次取数组中的一行
+        for x in X:
+            dis = np.sqrt(np.sum((x - self.X) ** 2,axis=1)) # 欧式距离，并将距离的倒数作为权重
+            # 返回排序前在序列中的位置，因为要返回的是根据距离评出的k个y
+            index = dis.argsort()
+            # 取距离最近的k个元素的索引
+            index = index[:self.k]
+            # 返回每个元素出现的次数，元素必须是非负整数,并将距离的倒数作为权重
+            count = np.bincount(self.y[index],weights=1/dis[index])
+            # 返回值最大的元素的索引
+            result.append(count.argmax())
+        return np.asarray(result)
+
+# 处理数据集 但直接采样可能会导致数据的"扎堆"，所以将各个类别提取出来分别采样
+t0 = data[data["class"] == 0]
+t1 = data[data["class"] == 1]
+t2 = data[data["class"] == 2]
+
+# 打乱顺序  random_state 随机种子 使得实验可还原
+t0 = t0.sample(len(t0),random_state=0)
+t1 = t1.sample(len(t1),random_state=0)
+t2 = t2.sample(len(t2),random_state=0)
+
+# 构建训练集与测试集
+train_X = pd.concat([t0.iloc[:40,:-1],t1.iloc[:40,:-1],t2.iloc[:40,:-1]],axis=0)
+train_y = pd.concat([t0.iloc[:40,-1],t1.iloc[:40,-1],t2.iloc[:40,-1]],axis=0)
+test_X = pd.concat([t0.iloc[40:,:-1],t1.iloc[40:,:-1],t2.iloc[40:,:-1]],axis=0)
+test_y = pd.concat([t0.iloc[40:,-1],t1.iloc[40:,-1],t2.iloc[40:,-1]],axis=0)
+
+# 创建对象，进行训练
+knn = KNN(3)
+knn.fit(train_X,train_y)
+# 进行测试
+result = knn.predict(test_X)
+print("预测结果：")
+print(result)
+print("真实结果：")
+print(np.asarray(test_y))
+print("准确率：")
+print(np.sum(result == test_y)/len(result))
+
+'''
+    KNN可视化显示
+'''
+
+# 使mpl支持中文显示
+mpl.rcParams["font.family"] = 'SinHei'
+# 防止使用中文负号
+mpl.rcParams["axes.unicode_minus"] = False
+
+# 由于有四个纬度，但在绘制散点图时，挑选其中两个纬度进行绘制
+plt.scatter(x=t0['sepallength'][:40],y=t0['petallength'][:40],color='r',label="Iris-virginica")
+plt.scatter(x=t1['sepallength'][:40],y=t1['petallength'][:40],color='b',label="Iris-setosa")
+plt.scatter(x=t2['sepallength'][:40],y=t2['petallength'][:40],color='g',label="Iris-versicolor")
+plt.show()
---
+++ KNN/KNN_regression.py
@@ -0,0 +1,78 @@
+import numpy as np
+import pandas as pd
+import matplotlib as mpl
+import matplotlib.pyplot as plt
+
+'''
+    使用KNN解决回归问题，使用鸢尾花的前三个属性，预测最后一个属性
+'''
+
+data = pd.read_csv(r"iris.arff.csv")
+# 删除不需要的列
+data = data.drop(["class"],axis=1)
+print(data)
+# 删除重复行
+data.drop_duplicates(inplace=True)
+print(data)
+
+# 使用KNN解决回归问题，使用鸢尾花的前三个属性，预测最后一个属性
+class KNN:
+    def __init__(self,k):
+        '''
+            :param k: int 邻居的个数
+            :return:
+        '''
+        self.k = k
+
+    def fit(self,X,y):
+        '''
+            KNN为惰性学习算法，他将数据集先保存起来，在新的数据到达时在进行判断与分类
+            :param X: 类数组类型 [样本数量，属性数目]
+            :param y: 类数组类型 [样本数量]
+            :return:
+        '''
+        self.X = np.asarray(X)
+        self.y = np.asarray(y)
+
+    def predict(self,X):
+        '''
+        根据参数传递的样本，对样本进行预测
+        :param X: 类数组类型 [样本数量，属性数目]
+        :return: 数组类型 预测结果
+        '''
+        X = np.asarray(X)
+        result = []
+        for x in X:
+            '''计算距离'''
+            dis = np.sqrt(np.sum((x - X) ** 2,axis=1))
+            # 排序后，每个元素，在原数组的索引
+            index = dis.argsort()
+            index = index[:self.k]
+            result.append(np.mean(self.y[index]))
+            # result.append(np.mean(np.multiply(self.y[index],1/dis[index])))
+        return result
+
+# 处理数据集
+t = data.sample(len(data),random_state=0)
+train_X = t.iloc[:120,:-1]
+train_y = t.iloc[:120,-1]
+test_X = t.iloc[120:,:-1]
+test_y = t.iloc[120:,-1]
+
+knn = KNN(6)
+knn.fit(X=train_X,y=train_y)
+result = knn.predict(test_X)
+print("预测输出：")
+print(result)
+
+print("真实输出：")
+print(np.asarray(test_y))
+
+print("误差：")
+print(np.sum(result-test_y) ** 2)
+
+# 调整画布
+plt.figure(figsize=(10,10))
+plt.plot(result,"ro-",label="result")
+plt.plot(test_y.values,"go-",label="true")
+plt.show()
